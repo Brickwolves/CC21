fd2a439c174dac4b46f05a27f80e4f9889575623
Owen <owentscooper@gmail.com>
2021-07-05 12:01:30 -0400

    added switch demos for lab 1.4

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lab14/Demo1Servo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lab14/Demo1Servo.java
@@ -13,12 +13,10 @@ import static org.firstinspires.ftc.teamcode.Utilities.OpModeUtils.multTelemetry
 
 
 @TeleOp(name="Servo Demo", group="Iterative Opmode")
-public class ServoDemo extends OpMode {
+public class Demo1Servo extends OpMode {
 	
-	/*
-		the declaration of servos is very similar to dc motors, the only difference
-		is changing the class from DcMotor to Servo.
-	*/
+	/*  the declaration of servos is very similar to dc motors, the only difference
+		is changing the class from DcMotor to Servo.    */
 	
 	DcMotor demoMotor;
 	
@@ -28,10 +26,8 @@ public class ServoDemo extends OpMode {
 	@Override
 	public void init() {
 		
-		/*
-			initialization for a servo is also very similar to a dc motor, the only thing
-			that's different is what class you input in the hardware map.
-		*/
+		/*  initialization for a servo is also very similar to a dc motor, the only thing
+			that's different is what class you input in the hardware map.   */
 		
 		demoMotor = hardwareMap.get(DcMotor.class, "demomotor");
 		
@@ -42,18 +38,14 @@ public class ServoDemo extends OpMode {
 	@Override
 	public void loop() {
 		
-		/*
-			for a dc motor you call the setPower() method and input a value from -1 to 1,
-			which tells the motor how fast to spin. 1 and -1 are full speed in each direction, 0 is stopped.
-		*/
+		/*  for a dc motor you call the setPower() method and input a value from -1 to 1,
+			which tells the motor how fast to spin. 1 and -1 are full speed in each direction, 0 is stopped.    */
 		
 		demoMotor.setPower(1.0);
 		
 		
-		/*
-			For servos, instead you call the setPosition() and input a value from 0 to 1,
-			which tells the servo what position to go to. It has a 300º range of motion
-		*/
+		/*  For servos, instead you call the setPosition() and input a value from 0 to 1,
+			which tells the servo what position to go to. It has a 300º range of motion */
 		
 		demoServo.setPosition(1.0);
 		
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lab14/Demo3SwitchConditions.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.lab14;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Hardware.Controller;
+
+
+@TeleOp(name="Switch Demo 2", group="Iterative Opmode")
+public class Demo3SwitchConditions extends OpMode {
+	
+	int demoInt = 0;
+	
+	Controller controller1;
+	
+	@Override
+	public void init() {
+		
+		controller1 = new Controller(gamepad1);
+		
+	}
+	
+	
+	@Override
+	public void loop() {
+		
+		switch (demoInt){
+			
+			case 1:
+				telemetry.addData("case", 1);
+				
+				if(controller1.circle.press()) demoInt = 2;
+				
+				break;
+			
+				
+			case 2:
+				telemetry.addData("case", 2);
+				
+				if(controller1.circle.press()) demoInt = 3;
+				
+				break;
+			
+				
+			case 3:
+				telemetry.addData("case", 3);
+				
+				if(controller1.circle.press()) demoInt = 1;
+				
+				break;
+			
+			
+		}
+		
+	}
+}
